9e2101e0739475524340c0580dd2294b3e7f9368
lkdhy <li2003xfkl@163.com>
2024-05-09 01:15:50 +0800

    提问对话框已造出来，准备后端启动

--- frontend/src/components/Homepage.vue
+++ frontend/src/components/Homepage.vue
@@ -4,19 +4,22 @@ import {useRouter} from 'vue-router';
 import {ElMessage} from 'element-plus';
 import {RegisterApi} from "@/request/api";
 import {useUserstore} from "@/store/user";
+import PostDialog from "@/components/PostDialog.vue";
 
 const router = useRouter();
 const userStore = useUserstore();
 
-console.log(`当前登录用户${userStore.userName}`)
+// console.log(`当前登录用户${userStore.userName}`)
 const curUser = ref<string>(userStore.userName);
-// const visUser = ref<string>('tmp');
+// const visUser = ref<string>($route.params.id);
+
+// console.log(visUser);
 
 </script>
 
 <template>
 
-  <h1>我是个人主页</h1>
+  <h2>我是个人主页</h2>
 
   <h3>
     你好 {{ curUser }}
@@ -25,8 +28,50 @@ const curUser = ref<string>(userStore.userName);
     这里是 {{ $route.params.id }} 的个人主页
   </h3>
 
+  <el-descriptions
+      title="User Info"
+      border
+  >
+    <el-descriptions-item label="Username">
+      {{ $route.params.id }}
+    </el-descriptions-item>
+    <el-descriptions-item label="姓名"></el-descriptions-item>
+    <el-descriptions-item label="Place">
+      这些东西后面再仔细搞！！！
+    </el-descriptions-item>
+    <el-descriptions-item label="Remarks">
+      <el-tag size="small">School</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="Address">
+<!--      No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province-->
+    </el-descriptions-item>
+  </el-descriptions>
+
+<!--  <el-button type="primary">-->
+<!--    向他提问-->
+<!--  </el-button>-->
+
+  <post-dialog>
+    <template v-slot:hostName>
+      {{ $route.params.id }}
+    </template>
+  </post-dialog>
+
+<!--  TODO  -->
+  <div>
+    <h2>
+      他的回答
+    </h2>
+    <p>
+      ……………………………………
+    </p>
+  </div>
+
 </template>
 
 <style scoped>
-
+/* TODO */
+* {
+  margin: 10px;
+}
 </style>
--- frontend/src/components/PostDialog.vue
+++ frontend/src/components/PostDialog.vue
@@ -1,30 +1,26 @@
 <template>
   <el-button @click="dialogVisible = true">点击我</el-button>
-  <el-dialog v-model="dialogVisible" title="填写表单">
-    <el-form ref="form" :model="form" label-width="80px">
+  <el-dialog
+      v-model="dialogVisible"
+  >
+    <template #header="{ titleClass }">
+      <h4>
+        你正在向<slot name="hostName"></slot>提问
+      </h4>
+    </template>
+    <p>
+      你想对他说：
+    </p>
+    <el-input
+        v-model="form.name"
+        type="textarea"
+        :autosize="{ minRows: 10, maxRows: 100 }"
+        placeholder="你想对他说……"
+    >
+    </el-input>
 
-<!--      <el-table>-->
-<!--        <el-table-column label="hhh"></el-table-column>-->
-<!--        <el-table-column label="fn2uon2o"></el-table-column>-->
-<!--      </el-table>-->
-      
-      <p>你好</p>
-      <a href="https://cs.fudan.edu.cn">复旦大学计算机科学技术学院</a>
-      
-      <el-form-item label="活动名称">
-        <el-input
-            v-model="form.name"
-            type="textarea"
-            maxlength="10"
-        ></el-input>
-      </el-form-item>
-
-      <el-form-item>
         <el-button type="primary" @click="submitForm('form')">立即创建</el-button>
         <el-button @click="dialogVisible = false">取消</el-button>
-      </el-form-item>
-    </el-form>
-
 
   </el-dialog>
 </template>
