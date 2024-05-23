134d1a2930ae992751b0908095ae20871e07e82f
lkdhy <li2003xfkl@163.com>
2024-05-23 20:50:36 +0800

    以Vue惯用写法修改部分组件，便于以后修改和维护

--- frontend/src/components/BoxCard.vue
+++ frontend/src/components/BoxCard.vue
@@ -1,59 +1,50 @@
-<script lang="ts">
+<script setup lang="ts">
 import { ref, reactive } from "vue";
 import type {FormInstance, FormRules} from 'element-plus';
 import {ElMessage, ElNotification as notify } from 'element-plus';
 import {PostApi} from "@/request/api";
 import {useUserstore} from "@/store/user";
+// props
+// const props = defineProps(['hostName'])
+const props = defineProps({
+  hostName: String
+})
 
-export default {
-  props: {
-    hostName: String
-  },
-  setup(props, context) {
-    const userStore = useUserstore();
-    const PostVisible = ref(false)
+const userStore = useUserstore();
+const PostVisible = ref(false)
+const ruleFormRef = ref<FormInstance>();
+const postForm = reactive({
+  username: userStore.userName,
+  question: '你想问TA…'
+});
 
-    const hostName = ref(props.hostName)
-    const ruleFormRef = ref<FormInstance>();
-    const postForm = reactive({
-      username: userStore.userName,
-      question: '你想问TA…'
-    });
-    const submitForm = (formEl: FormInstance | undefined) => {
-      if (!formEl) return;
-      console.log('准备post问题');
-      formEl.validate(async (valid) => {
-        if (valid) {
-          console.log('post问题表单验证通过，准备提交');
-          console.log(postForm);
-          let tmp = props.hostName != undefined ? props.hostName : "WTF_why?";
-          let res = await PostApi({
-            username: postForm.username,
-            hostUsername: tmp,
-            question: postForm.question
-          });
-          console.log('share结果', res);
-          if (res.success) {
-            console.log('share表单提交成功');
-            ElMessage({
-              message: '已向TA发送提问',
-              type: 'success'
-            });
-            context.emit('boxesGot', 'hhhhfhf');
-          } else {
-            console.log('WTF，post提问失败');
-          }
-        } else {
-          console.log('post表单验证不通过');
-        }
+const submitForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  console.log('准备post问题');
+  formEl.validate(async (valid) => {
+    if (valid) {
+      console.log('post问题表单验证通过，准备提交');
+      console.log(postForm);
+      let tmp = props.hostName != undefined ? props.hostName : "WTF_why?";
+      let res = await PostApi({
+        username: postForm.username,
+        hostUsername: tmp,
+        question: postForm.question
       });
+      // console.log('share结果', res);
+      if (res.success) {
+        console.log('share表单提交成功');
+        ElMessage({
+          message: '已向TA发送提问',
+          type: 'success'
+        });
+      } else {
+        console.log('WTF，post提问失败');
+      }
+    } else {
+      console.log('post表单验证不通过');
     }
-    return {
-      PostVisible, postForm,
-      submitForm, ruleFormRef,
-      hostName
-    };
-  }
+  });
 }
 
 </script>
@@ -65,7 +56,7 @@ export default {
         <el-tooltip
           content="点击向TA提问"
         >
-          <div @click="console.log(`按下了${hostName}`);
+          <div @click="console.log(`按下了“${hostName}”`);
               PostVisible = true;">
             <p><slot name="desc"></slot></p>
           </div>
